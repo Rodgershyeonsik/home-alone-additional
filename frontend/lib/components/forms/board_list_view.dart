a8adf589376905433567ad39b7c94cc24e80f3d6
HEEJI YUN <yhj8147e@gmail.com>
2023-06-02 15:37:34 +0900

    feat: 게시물 정렬 커스텀 dropdown 구현중

--- frontend/lib/components/forms/board_list_view.dart
+++ frontend/lib/components/forms/board_list_view.dart
@@ -9,7 +9,8 @@ import '../../pages/boards/board_detail_page.dart';
 import '../../utility/custom_enums.dart';
 
 class BoardListView extends StatefulWidget {
-  BoardListView ({Key? key, required this.boards, required this.listTitle}) : super(key: key);
+  BoardListView({Key? key, required this.boards, required this.listTitle})
+      : super(key: key);
 
   String listTitle;
   List<Board> boards;
@@ -19,83 +20,123 @@ class BoardListView extends StatefulWidget {
   State<BoardListView> createState() => _BoardListViewState();
 }
 
-
 class _BoardListViewState extends State<BoardListView> {
-
-  late String _dropdownValue =  widget.sortItems.first;
+  late String _sortValue = widget.sortItems.first;
+  bool sortButtonIsExpanded = false;
 
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.all(10.0),
-      child: Column(
-        children: [
-          Container(
-            padding: const EdgeInsets.all(10.0),
-            color: Colors.grey[100],
-            height: 55.0,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text( widget.listTitle,
-                style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 20)),
-                _buildSortDropdown()
-              ],
+        padding: const EdgeInsets.all(10.0),
+        child: Column(
+          children: [
+            Container(
+              padding: const EdgeInsets.all(10.0),
+              color: Colors.grey[100],
+              height: 55.0,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(widget.listTitle,
+                      style: const TextStyle(
+                          fontWeight: FontWeight.bold, fontSize: 20)),
+                  _buildSortButton()
+                ],
+              ),
             ),
-          ),
-          Expanded(
-            child: ListView.separated(
+            Expanded(
+                child: ListView.separated(
               itemCount: widget.boards.length,
               itemBuilder: (BuildContext context, int index) {
                 return ListTile(
-                    title: _makeBoard(widget.boards[index]),
+                  title: _makeBoard(widget.boards[index]),
                   contentPadding: EdgeInsets.symmetric(horizontal: 10.0),
                   onTap: () {
                     Navigator.push(
                       context,
                       MaterialPageRoute(
-                        builder: (context) => BoardDetailPage(board: widget.boards[index]),
+                        builder: (context) =>
+                            BoardDetailPage(board: widget.boards[index]),
                       ),
                     );
                   },
                 );
-              }, separatorBuilder: (BuildContext context, int index) => Divider(thickness: 1, height: 1),
-            )
-          )
-        ],
-      )
-    );
+              },
+              separatorBuilder: (BuildContext context, int index) =>
+                  Divider(thickness: 1, height: 1),
+            ))
+          ],
+        ));
   }
 
-  DropdownButton<String> _buildSortDropdown() {
-    return DropdownButton<String>(
-                elevation: 0,
-                  underline: Container(),
-                  iconSize: 30.0,
-                  isDense: true,
-                  dropdownColor: Colors.red,
-                  style: TextStyle(
-                    color: Colors.black87,
-                    fontSize: 15
-                  ),
-                  menuMaxHeight: 130,
-                  value: _dropdownValue,
-                  items: widget.sortItems.map<DropdownMenuItem<String>>((value) =>
-                  DropdownMenuItem<String>(
-                    value: value,
-                      child: Text(value))).toList(),
-                  onChanged: (value) {
-                    setState(() {
-                      _dropdownValue = value!;
-                      if(_dropdownValue == "최신순"){
-                        print('최신순 정렬됨?');
-                        Provider.of<BoardListProvider>(context, listen: false).sortBoard(SortBy.latest);
-                      } else {
-                        print('오래된 순 정렬됨?');
-                        Provider.of<BoardListProvider>(context, listen: false).sortBoard(SortBy.earliest);
-                      }
-                    });
-                  });
+  Widget _buildSortButton() {
+    return Container(
+      color: Colors.green,
+      width: 200,
+      height: 150.0,
+        padding: const EdgeInsets.symmetric(horizontal: 15.0),
+        child: Column(children: [
+          Row(
+            children: [
+              Text(_sortValue),
+              sortButtonIsExpanded
+                  ? Icon(Icons.arrow_drop_up)
+                  : Icon(Icons.arrow_drop_down),
+            ],
+          ),
+          sortButtonIsExpanded
+              ? Column(
+                  children: [
+                    TextButton(
+                        onPressed: () {
+                          setState(() {
+                            sortButtonIsExpanded = false;
+                          });
+                          Provider.of<BoardListProvider>(context,
+                                  listen: false)
+                              .sortBoard(SortBy.latest);
+                        },
+                        child: Text("최신순")),
+                    TextButton(
+                        onPressed: () {
+                          setState(() {
+                            sortButtonIsExpanded = false;
+                          });
+                          Provider.of<BoardListProvider>(context,
+                                  listen: false)
+                              .sortBoard(SortBy.earliest);
+                        },
+                        child: Text("오래된순")),
+                  ],
+                )
+              : const SizedBox.shrink()
+        ])
+        // child: DropdownButton<String>(
+        //             elevation: 0,
+        //               underline: Container(),
+        //               iconSize: 30.0,
+        //               style: TextStyle(
+        //                 color: Colors.black87,
+        //                 fontSize: 15
+        //               ),
+        //               value: _dropdownValue,
+        //               items: widget.sortItems.map<DropdownMenuItem<String>>((value) =>
+        //               DropdownMenuItem<String>(
+        //                 value: value,
+        //                   child: Text(value))).toList(),
+        //               onChanged: (value) {
+        //                 setState(() {
+        //                   _dropdownValue = value!;
+        //                   if(_dropdownValue == "최신순"){
+        //                     print('최신순 정렬됨?');
+        //                     Provider.of<BoardListProvider>(context, listen: false).sortBoard(SortBy.latest);
+        //                   } else {
+        //                     print('오래된 순 정렬됨?');
+        //                     Provider.of<BoardListProvider>(context, listen: false).sortBoard(SortBy.earliest);
+        //                   }
+        //                 });
+        //               }),
+        );
   }
 
   Widget _makeBoard(Board board) {
@@ -116,7 +157,8 @@ class _BoardListViewState extends State<BoardListView> {
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             crossAxisAlignment: CrossAxisAlignment.end,
             children: [
-              Text(splitRegDate[0] + ' ' + splitRegDate[1].substring(0,5),
+              Text(
+                splitRegDate[0] + ' ' + splitRegDate[1].substring(0, 5),
                 style: TextStyle(fontWeight: FontWeight.normal, fontSize: 10),
               ),
               Text(
