02ad7e6b31de411288646a7d513ded505e6a2ce1
HEEJI YUN <yhj8147e@gmail.com>
2023-06-05 23:43:42 +0900

    upgrade: SortBy enum을 enhanced enum으로 변경

--- frontend/lib/utility/custom_enums.dart
+++ frontend/lib/utility/custom_enums.dart
@@ -1,1 +1,11 @@
-enum SortBy { latest, earliest }
+enum SortBy {
+  latest(sortValue: "최신순"),
+  earliest(sortValue: "오래된순");
+
+  const SortBy({
+    required this.sortValue
+});
+
+  final String sortValue;
+
+}
